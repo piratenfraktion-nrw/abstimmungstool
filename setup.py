07c9f216d27e84645443ec745e3610ac2c220c50
Gerrit Giehl <ggiehl@piratenfraktion-nrw.de>
2014-11-16 12:46:31 +0100

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -9,20 +9,20 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "an_example_pypi_project",
-    version = "0.0.4",
-    author = "Andrew Carter",
-    author_email = "andrewjcarter@gmail.com",
-    description = ("An demonstration of how to create, document, and publish "
-                                   "to the cheese shop a5 pypi.org."),
-    license = "BSD",
-    keywords = "example documentation tutorial",
-    url = "http://packages.python.org/an_example_pypi_project",
-    packages=['an_example_pypi_project', 'tests'],
+    name = "abstimmungstool",
+    version = "0.0.1",
+    author = "Gerrit Giehl",
+    author_email = "ggiehl@piratenfraktion-nrw.de",
+    description = (""
+                   ""),
+    license = "AGPL",
+    keywords = "abstimmungstool",
+    url = "https://github.com/piratenfraktion-nrw/abstimmungstool",
+    packages=['abstimmungstool', 'tests'],
     long_description=read('README'),
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 1 - Alpha",
         "Topic :: Utilities",
-        "License :: OSI Approved :: BSD License",
+        "License :: OSI Approved :: AGPL License",
     ],
 )
